1c7f3bf0393af036cd29230783469ed91ac1b058
Daniel silva rocha <danielddsilva2@hotmail.com>
2025-03-27 20:57:24 -0300

    :card_file_box: raw: changed location of frontend.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
-    {
-        return view('welcome_message');
+    public function index(): string{
+        $result = file_get_contents("../public/index.html");
+        if(!$result){
+            $this->response->setStatusCode(500);
+            return "500 Internal Error";
+        }
+        return $result;
     }
 }
